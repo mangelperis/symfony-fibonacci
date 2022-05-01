24cbe888e7550f8ee507bd9d7c8933c18958bbcc
Miguel Angel <miguel.peris@witrac.es>
2022-05-01 23:49:07 +0200

    Using new classes names into tests also

--- src/tests/FibonacciTest.php
+++ src/tests/FibonacciTest.php
@@ -2,31 +2,36 @@
 
 namespace App\Tests;
 
-use App\Repository\FibonacciRepository;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Services\FibonacciService;
+use App\Services\Helpers;
+use PHPUnit\Framework\TestCase;
 
-class FibonacciTest extends WebTestCase
+class FibonacciTest extends TestCase
 {
-    private $fibonacciRepository;
+    private $fibonacciService;
 
     protected function setUp(): void
     {
-        $this->fibonacciRepository = new FibonacciRepository();
+        $this->fibonacciService = new FibonacciService();
+
     }
 
     public function testStringValidatorHelper()
     {
         $validDate = '2022-03-01 00:00:00';
-        $this->assertTrue($this->fibonacciRepository->isStringValidDate($validDate));
+        $this->assertTrue(Helpers::isStringValidDate($validDate));
 
         $noHisDate = '2022-03-01';
-        $this->assertFalse($this->fibonacciRepository->isStringValidDate($noHisDate));
+        $this->assertFalse(Helpers::isStringValidDate($noHisDate));
 
         $invalidFormatDate = '2022/03/01 00:00:00';
-        $this->assertFalse($this->fibonacciRepository->isStringValidDate($invalidFormatDate));
+        $this->assertFalse(Helpers::isStringValidDate($invalidFormatDate));
     }
 
 
+    /**
+     * @throws \Exception
+     */
     public
     function testApplicationGenerator()
     {
@@ -34,13 +39,41 @@ class FibonacciTest extends WebTestCase
         $from = '1970-01-01 00:00:10';
         $to = '1970-01-01 00:01:40';
 
-        $result = $this->fibonacciRepository->numbersBetweenDates($from, $to);
+        $result = $this->fibonacciService->numbersBetweenDates($from, $to);
 
         $this->assertIsArray($result);
         $this->assertCount(5, $result);
     }
 
+    /**
+     * @throws \Exception
+     */
+    public
+    function testApplicationGenerator2()
+    {
+        //There are five Fibonacci numbers in given range (10-100)
+        $from = '1970-01-01 00:00:10';
+        $to = '1970-01-01 00:01:40';
 
+        $helpersMock = $this->createMock(Helpers::class);
+
+        $helpersMock->method('isStringValidDate2')->willReturn(true);
+
+        $result = $this->fibonacciService->numbersBetweenDates($from, $to);
+
+        $helpersMock->expects($this->exactly(1))->method('isStringValidDate2')->with(
+            'from'
+        );
+        $helpersMock->expects($this->exactly(1))->method('isStringValidDate2')->with(
+            'to'
+        );
+
+    }
+
+
+    /**
+     * @throws \Exception
+     */
     public
     function testBadParamsExec()
     {
@@ -48,7 +81,7 @@ class FibonacciTest extends WebTestCase
         $to = '1970-01-01 00:01:40';
 
         $this->expectExceptionCode(400);
-        $result = $this->fibonacciRepository->numbersBetweenDates($from, $to);
+        $result = $this->fibonacciService->numbersBetweenDates($from, $to);
 
         $this->assertIsNotArray($result);
     }
